efe99e1e91d296bb9b77028a0002b6f94c8cc913
Daniel Melo <daniel.melo42@outlook.com>
2023-01-24 09:01:29 -0700

    di for TeamManagerApi

--- app/src/main/java/com/dicedev/soccerteammanager/di/AppModule.kt
+++ app/src/main/java/com/dicedev/soccerteammanager/di/AppModule.kt
@@ -1,10 +1,28 @@
 package com.dicedev.soccerteammanager.di
 
+import android.content.Context
+import com.dicedev.soccerteammanager.network.TeamManagerApi
+import com.dicedev.soccerteammanager.utils.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTeamManagerAPI(): TeamManagerApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(TeamManagerApi::class.java)
+    }
 }
