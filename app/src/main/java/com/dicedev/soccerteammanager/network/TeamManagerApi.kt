edc7374c06183b7e4d40b140e49122c39e784839
Daniel Melo <daniel.melo42@outlook.com>
2023-01-24 09:22:45 -0700

    imports refactored

--- app/src/main/java/com/dicedev/soccerteammanager/network/TeamManagerApi.kt
+++ app/src/main/java/com/dicedev/soccerteammanager/network/TeamManagerApi.kt
@@ -2,13 +2,7 @@ package com.dicedev.soccerteammanager.network
 
 import com.dicedev.soccerteammanager.models.Team
 import com.dicedev.soccerteammanager.models.TeamList
-import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface TeamManagerApi {
 
