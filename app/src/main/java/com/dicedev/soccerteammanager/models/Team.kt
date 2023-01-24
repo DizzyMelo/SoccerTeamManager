f61f9394cc46e5c6a19cf7a9ea4046944d985b53
Daniel Melo <daniel.melo42@outlook.com>
2023-01-24 08:59:39 -0700

    team models created

---
+++ app/src/main/java/com/dicedev/soccerteammanager/models/Team.kt
@@ -0,0 +1,8 @@
+package com.dicedev.soccerteammanager.models
+
+data class Team(
+    val _id: String,
+    val dateCreated: String,
+    val league: String,
+    val name: String
+)
---
+++ app/src/main/java/com/dicedev/soccerteammanager/models/TeamList.kt
@@ -0,0 +1,6 @@
+package com.dicedev.soccerteammanager.models
+
+data class TeamList(
+    val results: Int,
+    val teams: List<Team>
+)
