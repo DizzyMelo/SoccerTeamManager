393d1232a060b71b0805a2c1bd9406b29a337f3d
Daniel Melo <daniel.melo42@outlook.com>
2023-01-24 09:23:57 -0700

    screen created

---
+++ app/src/main/java/com/dicedev/soccerteammanager/screens/team/CreateTeamScreen.kt
@@ -0,0 +1,10 @@
+package com.dicedev.soccerteammanager.screens.team
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun CreateTeamScreen(navController: NavController) {
+    Text(text = "Create Team Screen")
+}
